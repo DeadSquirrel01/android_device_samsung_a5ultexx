f81781e2e7e7f3e05ba7a46710504fde98bd9bf0
DeadSquirrel01 <m.marcaccini01@gmail.com>
2016-10-16 09:37:57 +0200

    Updated no-kexec flag and switched to eng

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -128,7 +128,7 @@ MR_DPI_FONT := 216
 MR_KEXEC_MEM_MIN := 0x85000000
 MR_FSTAB := device/samsung/a5ultexx/multirom/mrom.fstab
 MR_USE_MROM_FSTAB := true
-MR_ALLOW_NKK71_NOKEXEC_WORKAROUND := true
+MR_NO_KEXEC := 4
 MR_CONTINUOUS_FB_UPDATE := true
 
 # Dex
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo pac_a5ultexx-userdebug
+add_lunch_combo pac_a5ultexx-eng
