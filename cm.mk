b6cd608ddda53332edb2b29b4b78b6c062f9667e
DeadSquirrel01 <m.marcaccini01@gmail.com>
2016-11-27 00:44:38 +0100

    Removed NFC Enhanced no longer needed

--- cm.mk
+++ cm.mk
@@ -2,9 +2,6 @@
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 include vendor/cm/config/common.mk
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 $(call inherit-product, device/samsung/a5ultexx/cm_a5ultexx.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=a5ultexx TARGET_DEVICE=a5ulte
