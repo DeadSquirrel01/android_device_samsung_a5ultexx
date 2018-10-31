43ca1617957244bb322be302a192c652014e0619
DeadSquirrel01 <m.marcaccini01@gmail.com>
2018-10-31 18:43:36 +0100

    Add twrp fstab

--- device.mk
+++ device.mk
@@ -45,3 +45,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fsck.f2fs \
     mkfs.f2fs
+
+# For TWRP
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/recovery/twrp.fstab:recovery/root/etc/twrp.fstab
